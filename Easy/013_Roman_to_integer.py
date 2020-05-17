4c3f80662d38aa5da743cf0099647a2faa99036d
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-05-17 16:30:11 +0100

    Update 013_Roman_to_integer.py

--- Easy/013_Roman_to_integer.py
+++ Easy/013_Roman_to_integer.py
@@ -2,6 +2,9 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         '''Given a roman numeral, convert it to an integer
         
+        Example. Input: "MCMXCIV". Output: 1994
+        Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+        
         '''
     
         dics = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}        
