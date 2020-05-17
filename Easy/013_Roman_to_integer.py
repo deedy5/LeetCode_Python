e444ed27369bf3e05d13bcee57f3d44f1611eb0f
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-05-17 16:12:27 +0100

    Create 013_Roman_to_integer.py

---
+++ Easy/013_Roman_to_integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+    
+        dics = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}        
+        summ = 0
+        t = 1001
+        
+        for x in s:
+            i = dics[x]
+            if i <= t:
+                summ += i
+            else:
+                summ += (i - t) - t
+            t = i
+            
+        return summ
