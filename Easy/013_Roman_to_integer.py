b49252020f2770578d1f811e6323aa0f96e7b619
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-05-17 16:28:33 +0100

    Update 013_Roman_to_integer.py

--- Easy/013_Roman_to_integer.py
+++ Easy/013_Roman_to_integer.py
@@ -1,6 +1,8 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        '''Given a roman numeral, convert it to an integer'''
+        '''Given a roman numeral, convert it to an integer
+        
+        '''
     
         dics = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}        
         summ = 0
